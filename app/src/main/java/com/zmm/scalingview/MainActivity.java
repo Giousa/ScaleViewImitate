9676a515edd8c01d91e1bde0d807f3b12d1c5f95
Giousa <65489469@qq.com>
2017-03-30 14:27:59 +0800

    first commit

---
+++ app/src/main/java/com/zmm/scalingview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.zmm.scalingview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ScrollView mScrollView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+        mScrollView = (ScrollView) findViewById(R.id.scroll_view);
+        Button scale = (Button) findViewById(R.id.btn_scale);
+
+        scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/zmm/scalingview/SecondActivity.java
@@ -0,0 +1,37 @@
+package com.zmm.scalingview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Description:
+ * Author:zhangmengmeng
+ * Date:2017/3/30
+ * Time:下午2:19
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        initView();
+    }
+
+    private void initView() {
+        Button scale = (Button) findViewById(R.id.btn_scale);
+
+        scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
+    }
+}
